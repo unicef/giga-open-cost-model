b222b024e66f35723c0ea957e7fa3645e1e8badc
Nathan Eliason <nathan.eliason@actualhq.com>
2023-05-12 05:18:16 +0000

    Add component and scenario unit tests for P2P

--- test/models/test_components.py
+++ test/models/test_components.py
@@ -9,6 +9,8 @@ from giga.models.components.satellite_cost_model import SatelliteCostModel
 from giga.schemas.conf.models import SatelliteTechnologyCostConf
 from giga.models.components.cellular_cost_model import CellularCostModel
 from giga.schemas.conf.models import CellularTechnologyCostConf
+from giga.models.components.p2p_cost_model import P2PCostModel
+from giga.schemas.conf.models import P2PTechnologyCostConf
 from giga.data.space.model_data_space import ModelDataSpace
 
 
@@ -82,6 +84,25 @@ def cellular_config():
         },
     )
 
+@pytest.fixture()
+def p2p_config():
+    return P2PTechnologyCostConf(
+        capex={"tower_fixed_costs":100.0},
+        opex={
+            "fixed_costs": 0.0,
+            "annual_bandwidth_cost_per_mbps": 10.0
+        },
+        constraints={
+            "maximum_bandwithd": 300.0,
+            "required_power": 10.0,
+            "maximum_range": 800_000,
+        },
+        electricity_config={
+            "capex": {"solar_panel_costs": 500.0, "battery_costs": 0.0},
+            "opex": {"cost_per_kwh": 0.1},
+        },
+    )
+
 
 @pytest.fixture()
 def data_space(global_config):
@@ -120,4 +141,13 @@ def test_cellular_component(cellular_config, data_space):
     assert all([r.technology == "Cellular" for r in outputs.cost_results])
     assert len(outputs.cost_results) == 50  # 50 schools checked
     assert outputs.cost_results[0].capex == 500.0  # Fixed install cost
+    assert outputs.cost_results[0].opex == 200.0  # Operating cost
+
+def test_p2p_component(p2p_config, data_space):
+    # create and run the mode
+    model = P2PCostModel(p2p_config)
+    outputs = model.run(data_space)
+    assert all([r.technology == "P2P" for r in outputs.cost_results])
+    assert len(outputs.cost_results) == 50  # 50 schools checked
+    assert outputs.cost_results[0].capex == 100.0  # Fixed install cost
     assert outputs.cost_results[0].opex == 200.0  # Operating cost
