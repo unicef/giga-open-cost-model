ee4166dcd1033097c29567fdb28f4b0b80aab5ee
Nathan Eliason <nathan.eliason@actualhq.com>
2023-06-07 17:32:12 -0400

    Add map and graph outputs to PDF and ZIP exports

--- giga/viz/notebooks/components/widgets/giga_export.py
+++ giga/viz/notebooks/components/widgets/giga_export.py
@@ -1,9 +1,22 @@
 import base64
+import tempfile
+from selenium import webdriver
 from IPython.display import HTML, display
-from ipywidgets import Output, VBox
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch
+from ipywidgets import Output, VBox, Button, Widget
+from ipywidgets.embed import embed_minimal_html
 import io
+import base64
+import folium
+import os
+import shutil
+import plotly
+import time
 from zipfile import ZipFile
-from PyPDF2 import PdfWriter
+from PIL import Image
+
+from giga.utils.progress_bar import progress_bar as pb
 
 
 def make_export_cost_button(df, title="Export Costs", filename="costs.csv"):
@@ -53,82 +66,110 @@ def make_export_config_button(
         display(HTML(html))
     return out
 
+PAGE_WIDTH = 8 * inch
+PAGE_HEIGHT = 6 * inch
 
-def make_export_zip_button(title="Download Graph .zip", filename="graphs.zip"):
-    zip_buffer = io.BytesIO()
-    with ZipFile(zip_buffer, "w") as zip_file:
-        pass
+def render_screenshot(tmpfile):
+    # Adjust zoom to crop closer to the center of the web contents
+    zoom_scale = 1.5
+    try:
+        browser = webdriver.Chrome()
+    except:
+        raise Exception("Error: Google Chrome must be installed in order to turn graphs into PDFs in the background.")
+    browser.set_window_size(PAGE_WIDTH * zoom_scale, PAGE_HEIGHT * zoom_scale)
+    browser.get("file://" + tmpfile)
+    time.sleep(0.2)
+    png_bytes = browser.get_screenshot_as_png()
+    browser.quit()
+    return png_bytes
 
-    zip_buffer.seek(0)
-    b64 = base64.b64encode(zip_buffer.read())
-    payload = b64.decode()
-
-    html = f"""
-    <html>
-    <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-    </head>
-    <body>
-        <a download="{filename}" href="data:application/zip;base64,{payload}" download>
-            <button class="custom-button">{title}</button>
-        </a>
-    </body>
-    </html>
-    """
-    out = Output()
-    with out:
-        display(HTML(html))
-    return out
-
-
-def make_export_report_button(df, title="Export Report", filename="report.pdf"):
+def generate_pdf_bytes(el_list):
+    # Create a scratch temp file
+    tmpdir = tempfile.mkdtemp()
+    tmpfile = os.path.join(tmpdir, "tmp.html")
+    
     output = io.BytesIO()
-    pdf_writer = PdfWriter()
-    pdf_writer.add_blank_page(width=72, height=72)
-    with output:
-        pdf_writer.write(output)
-        pdf_bytes = output.getvalue()
-    b64 = base64.b64encode(pdf_bytes)
-    payload = b64.decode()
+    pdf_canvas = canvas.Canvas(output, pagesize=(PAGE_WIDTH, PAGE_HEIGHT))
 
-    html = """
-        <html>
-        <head>
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <style>
-                .custom-button {{
-                    background-color: #ffe3c9;
-                    border: 1px solid #ff9f40;
-                    color: #474747;
-                    padding: 8px 16px;
-                    text-align: center;
-                    text-decoration: none;
-                    display: inline-block;
-                    font-size: 12px;
-                    margin: 6px;
-                    cursor: pointer;
-                    border-radius: 2px;
-                    width: 150px;
-                }}
-            </style>
-        </head>
-        <body>
-            <a download="{filename}" href="data:application/pdf;base64,{payload}" download>
-                <button class="custom-button">{title}</button>
-            </a>
-        </body>
-        </html>
-    """
-    html = html.format(payload=payload, title=title, filename=filename)
+    has_content = False
+    iter_list = el_list if len(el_list) < 5 else pb(el_list)
+    for el in iter_list:
+        if isinstance(el, folium.Map):
+            el.save(tmpfile)
+            png_bytes = render_screenshot(tmpfile)
+        elif isinstance(el, plotly.graph_objs._figure.Figure):
+            png_bytes = plotly.io.to_image(el, format='png', 
+                                           width=PAGE_WIDTH * 2, height=PAGE_HEIGHT * 2,
+                                           scale=3)
+        else:
+            continue
+        has_content = True
+        png = Image.open(io.BytesIO(png_bytes))
+        pdf_canvas.drawInlineImage(png, 0, 0, width=PAGE_WIDTH, height=PAGE_HEIGHT)
+        pdf_canvas.showPage()
+
+    # Delete temporary directory
+    shutil.rmtree(tmpdir)
+
+    if not has_content:
+        return None
+    pdf_canvas.save()
+    return output.getvalue()
+
+def make_export_report_button(all_output_maps, title="Generate Report", filename="report.pdf"):
+    def on_button_clicked(b):
+        out.clear_output()
+        with out:
+            pdf_bytes = generate_pdf_bytes(all_output_maps.get_all())
+            b64 = base64.b64encode(pdf_bytes)
+            payload = b64.decode()
+            html = """
+                <a download="{filename}" href="data:application/pdf;base64,{payload}" download>
+                    <button class="custom-button">{title}</button>
+                </a>
+            """
+            html = html.format(payload=payload, title="Download Generated Report", filename=filename)
+            display(HTML(html))
+
+    button = Button(description=title)
+    button.on_click(on_button_clicked)
     out = Output()
-    with out:
-        display(HTML(html))
-    return out
+    return VBox([button, out])
+
+def make_export_zip_button(all_output_maps, title="Download Graph .zip", filename="graphs.zip"):
+    def on_button_clicked(b):
+        out.clear_output()
+        with out:
+            el_list = all_output_maps.get_all()
+            zip_buffer = io.BytesIO()
+            with ZipFile(zip_buffer, "w") as zip_file:
+                i = 0
+                for el in pb(el_list):
+                    pdf_bytes = generate_pdf_bytes([el])
+                    if pdf_bytes is None:
+                        continue
+                    zip_file.writestr(f"graph_{i}.pdf", pdf_bytes)
+                    i += 1
+            zip_buffer.seek(0)
+            b64 = base64.b64encode(zip_buffer.read())
+            payload = b64.decode()
+            html = """
+                <a download="{filename}" href="data:application/zip;base64,{payload}" download>
+                    <button class="custom-button">{title}</button>
+                </a>
+            """
+            html = html.format(payload=payload, title=title, filename=filename)
+            display(HTML(html))
+
+    button = Button(description=title)
+    button.on_click(on_button_clicked)
+    out = Output()
+    return VBox([button, out])
 
 
-def make_export_button_row(table, inputs):
+def make_export_button_row(table, inputs, all_output_maps):
     b1 = make_export_config_button(inputs)
     b2 = make_export_cost_button(table)
-    b3 = make_export_report_button(table)
-    b4 = make_export_zip_button()
+    b3 = make_export_report_button(all_output_maps)
+    b4 = make_export_zip_button(all_output_maps)
     return VBox([b1, b2, b3, b4])
