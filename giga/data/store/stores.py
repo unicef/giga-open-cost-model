197e60f238d18f6688d1495abaf9f5c96197ad58
Brian Musisi <brianmusisi@berkeley.edu>
2023-06-16 16:29:57 +0300

    Use Local Store

--- giga/data/store/stores.py
+++ giga/data/store/stores.py
@@ -5,7 +5,7 @@ from .local_fs_store import LocalFS
 
 # Global Data Store instances. 
 LOCAL_FS_STORE: DataStore = LocalFS()
-GCS_BUCKET_STORE: DataStore = GCSDataStore()
+GCS_BUCKET_STORE: DataStore = None  # GCSDataStore()
 
 # Configure which storage to use for country data here.
-COUNTRY_DATA_STORE: DataStore = GCS_BUCKET_STORE
+COUNTRY_DATA_STORE: DataStore = LOCAL_FS_STORE  #  GCS_BUCKET_STORE
