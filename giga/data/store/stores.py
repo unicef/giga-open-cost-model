6b0ea9c610742b0aed8a116a9374a0765aaef5f5
Brian Musisi <brianmusisi@berkeley.edu>
2023-07-06 17:43:26 +0300

    Switch from Local Storage to Blob Storage

--- giga/data/store/stores.py
+++ giga/data/store/stores.py
@@ -1,11 +1,13 @@
 
 from .data_store import DataStore
 from .gcs_store import GCSDataStore
+from .adls_store import ADLSDataStore
 from .local_fs_store import LocalFS
 
 # Global Data Store instances. 
 LOCAL_FS_STORE: DataStore = LocalFS()
 GCS_BUCKET_STORE: DataStore = None  # GCSDataStore()
+ADLS_CONTAINER_STORE: DataStore = ADLSDataStore()
 
 # Configure which storage to use for country data here.
-COUNTRY_DATA_STORE: DataStore = LOCAL_FS_STORE  #  GCS_BUCKET_STORE
+COUNTRY_DATA_STORE: DataStore = ADLS_CONTAINER_STORE()  # LOCAL_FS_STORE  #  GCS_BUCKET_STORE
